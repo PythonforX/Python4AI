c8a981e78a2af003bde4e0d8339663ce86f89d69
nirakar24 <jenashubham60@gmail.com>
2024-06-21 22:51:10 +0530

    Variables

---
+++ day_01/arithematic.py
@@ -0,0 +1,14 @@
+x=67
+y=76
+#addition
+print(x+y)
+#subtraction
+print(x-y)
+#multiplication
+print(x*y)
+#division
+print(x/y)
+#floor division
+print(x//y)
+#Modulus
+print(x%y)
---
+++ day_01/variables.py
@@ -0,0 +1,13 @@
+x = 45
+name = "john"
+is_student = False
+print(type(x))
+print(type(name))
+print(type(is_student))
+
+#float
+y = 6.7
+print(type(y))
+#complex
+z = 4 + 6j
+print(type(z))
