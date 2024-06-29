740bd8510ff382a496b763d9379db90f7e804260
nirakar24 <jenashubham60@gmail.com>
2024-06-29 15:25:36 +0530

    Day 06

---
+++ day_06/custom_module.py
@@ -0,0 +1,6 @@
+def area(width, height):
+    area_rectangle = width * height
+
+    return area_rectangle
+
+pi = 3.14
---
+++ day_06/new.py
@@ -0,0 +1,15 @@
+import math
+
+square_root = math.sqrt(25)
+# print(square_root)
+# print(custom_module.pi)
+
+
+def is_palindrome(s):
+    return s == s[::-1]
+
+print(is_palindrome("hello"))  # This will output: False
+
+
+
+
